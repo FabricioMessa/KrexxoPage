c51eb5599cc207c055027b78f4e9411084489ee7
Fabricio <fabricio.messa@ucsp.edu.pe>
2025-01-04 21:33:31 -0500

    cambio3

--- server.js
+++ server.js
@@ -2,23 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Middleware para servir archivos estáticos
+// Middleware para servir archivos estáticos desde la carpeta "public"
 app.use(express.static('public'));
-
-// Ruta principal
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-// Ruta para personajes
-app.get('/characters.html', (req, res) => {
-  res.sendFile(__dirname + '/characters.html');
-});
-
-// Ruta para imágenes aleatorias
-app.get('/random.html', (req, res) => {
-  res.sendFile(__dirname + '/random.html');
-});
 
 // Iniciar el servidor
 app.listen(port, () => {
