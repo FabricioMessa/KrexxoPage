b5842bdd133f103bc6d157f811a84dbc62a09a17
FabricioMessa <fabricio.messa@ucsp.edu.pe>
2025-02-20 22:29:14 -0500

    cambios30

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs'); // Cambiamos de bcrypt a bcryptjs
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
