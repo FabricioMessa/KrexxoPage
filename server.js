b6bcdeb4b3a2c97d29e197fe7739d04976085602
FabricioMessa <fabricio.messa@ucsp.edu.pe>
2025-02-20 23:34:20 -0500

    cambios32

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const authRoutes = require("./routes/auth");
 
 dotenv.config();
